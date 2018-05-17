a580c7aa00ae775aed4023d006f7442a600823c0
Shogo Ishikura <ishikura.shogo@gmail.com>
2018-05-18 03:02:04 +0900

    update

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
 var app = require('http').createServer(handler),
     io = require('socket.io').listen(app),
     fs = require('fs');
-
 app.listen(1337);
 
 function handler(req, res) {
-  fs.readFile(__dirname + '/index.html', function(err, data){
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error');
-    }
-    res.writeHead(200);
-    res.write(data);
-    res.end();
-  });
+  var url = req.url;
+  var tmp = url.split('.');
+  var ext = tmp[tmp.length - 1];
+
+  switch(ext){
+    case 'css':
+      fs.readFile(__dirname + '/' + url, function(err, data){
+        if (err) {
+          res.writeHead(500);
+          return res.end('Error');
+        }
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+      });
+      break;
+    default:
+      fs.readFile(__dirname + '/index.html', function(err, data){
+        if (err) {
+          res.writeHead(500);
+          return res.end('Error');
+        }
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+      });
+  }
 }
+io.sockets.on('connection', function(socket){
+  socket.on('emit_left_from_client', function(data){
+    socket.broadcast.emit('emit_left_from_server', data);
+  });
+  socket.on('emit_right_from_client', function(data){
+    socket.broadcast.emit('emit_right_from_server', data);
+  });
+});

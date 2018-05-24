ebb9d794aabee87f3dfea8467d1931d5e183a54d
Shogo Ishikura <ishikura.shogo@gmail.com>
2018-05-25 00:17:07 +0900

    redis導入、データ保存機能実装

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var redis = require('redis').createClient();
+
 var app = require('http').createServer(handler),
     io = require('socket.io').listen(app),
     fs = require('fs');
@@ -45,18 +47,29 @@ function handler(req, res) {
       });
   }
 }
+
 io.sockets.on('connection', function(socket){
   socket.on('connect_room_from_client', function(data){
     socket.join(data.room);
+
+    redis.get(data.room + "_left", function(err, val){
+      io.to(socket.id).emit('init_left_from_server', val ? val : "");
+    });
+
+    redis.get(data.room + "_right", function(err, val){
+      io.to(socket.id).emit('init_right_from_server', val ? val : "");
+    });
   });
 
   socket.on('emit_left_from_client', function(data){
     socket.join(data.room);
     socket.broadcast.to(data.room).emit('emit_left_from_server', data);
+    redis.set(data.room + "_left", data.body, function(){});
   });
 
   socket.on('emit_right_from_client', function(data){
     socket.join(data.room);
     socket.broadcast.to(data.room).emit('emit_right_from_server', data);
+    redis.set(data.room + "_right", data.body, function(){});
   });
 });
